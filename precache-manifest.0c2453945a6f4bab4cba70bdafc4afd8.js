e100f5a616e7e32cfb3ab3e17d97ec26e2a27286
Aaron Marco Arias <47487720+thegodofnoise@users.noreply.github.com>
2019-11-19 11:28:55 -0300

    Updates

---
+++ precache-manifest.0c2453945a6f4bab4cba70bdafc4afd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1441860d9dfd607d804e446b28946343",
+    "url": "/criptobingo/index.html"
+  },
+  {
+    "revision": "d1c5906ab09e07706668",
+    "url": "/criptobingo/static/css/main.d30d4e28.chunk.css"
+  },
+  {
+    "revision": "b8822d663b4092ad93e3",
+    "url": "/criptobingo/static/js/2.91c8dce1.chunk.js"
+  },
+  {
+    "revision": "d1c5906ab09e07706668",
+    "url": "/criptobingo/static/js/main.b713ef57.chunk.js"
+  },
+  {
+    "revision": "993960c16078c1b1ab02",
+    "url": "/criptobingo/static/js/runtime-main.42bd8f26.js"
+  },
+  {
+    "revision": "4770b86bfebb13a340e5b798d365b6c1",
+    "url": "/criptobingo/static/media/logo.4770b86b.png"
+  },
+  {
+    "revision": "ad5419e659c76880a35b3661159a54d5",
+    "url": "/criptobingo/static/media/siete.ad5419e6.png"
+  }
+]);
