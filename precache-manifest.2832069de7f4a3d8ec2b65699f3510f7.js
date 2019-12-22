4353418a1d68c86980d9ce6d632137479dfd1817
Aaron Marco Arias <47487720+thegodofnoise@users.noreply.github.com>
2019-12-22 17:13:47 -0300

    Actualización desde nuevo "build"

---
+++ precache-manifest.2832069de7f4a3d8ec2b65699f3510f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4415ae6027eab1042caf68e40148f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5132ccb2bd11ff61f78a",
+    "url": "/static/css/main.fe895f11.chunk.css"
+  },
+  {
+    "revision": "2658964082384d9a1722",
+    "url": "/static/js/2.f68b2487.chunk.js"
+  },
+  {
+    "revision": "5132ccb2bd11ff61f78a",
+    "url": "/static/js/main.a957ac9b.chunk.js"
+  },
+  {
+    "revision": "eff9f9948f52239a4859",
+    "url": "/static/js/runtime-main.d0004f7f.js"
+  },
+  {
+    "revision": "4770b86bfebb13a340e5b798d365b6c1",
+    "url": "/static/media/logo.4770b86b.png"
+  }
+]);
